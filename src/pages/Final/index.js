78c83a15db890674a2241ef7f391438afa8c42d9
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-08-04 20:49:29 -0300

    jogo efetivo

--- src/pages/Final/index.js
+++ src/pages/Final/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import LogoMhedica from "../../components/logo-mhedica";
 import LogoMhedTech from "../../components/logo-mhedtech/logomhedtech";
-import bau from '../../assets/img/Mapa colorido.svg'
+import mapa from '../../assets/img/Mapa colorido.svg'
+import bau from '../../assets/img/BAÚ1.svg'
 
 export default function Final(){
 
@@ -12,13 +13,14 @@ export default function Final(){
                     <LogoMhedica />
                     <h1>Caça ao Tesouro Mhédica</h1>
                     <h2>Parabéns você chegou na ultima etapa da busca!</h2>
-                    <img className="img animation-grow" alt="Imagem Bau" src={bau}></img>
+                    <img className="img animation-grow" alt="Imagem Bau" src={mapa}></img>
                     <div className="content">
                         <p>Você percorreu todos os caminhos e chegou e agora vai saber aonde o tesouro se encontra!</p>
                         <p>Ao longo do caminho, você recebeu pistas nos locais visitados.</p>
                         <p>Duas delas formam o nome do setor aonde está o tesouro.</p>
                         <p>Junte as duas e diga no local para desenterra-lo!</p>
                     </div>
+                    <img className="img animation-grow" alt="Imagem Bau" src={bau}></img>
                     <LogoMhedTech />
                 </div>
             </div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import {Link} from "react-router-dom";
 import LogoMhedica from "../../components/logo-mhedica";
 import LogoMhedTech from "../../components/logo-mhedtech/logomhedtech";
-import bau from '../../assets/img/mapa-1.svg'
+import mapa from '../../assets/img/mapa-1.svg';
+import bau from '../../assets/img/BAÚ4.svg'
 import './home.css';
 
 export default function Home(){
@@ -13,15 +14,15 @@ export default function Home(){
                     <LogoMhedica />
                     <h1>Caça ao Tesouro Mhédica</h1>
                     <h2>Você foi convocado para uma aventura!</h2>
-                    <img className="img animation-grow" alt="Imagem Bau" src={bau}></img>
+                    <img className="img-mapa animation-down" alt="Imagem Bau" src={mapa}></img>
                     <div className="content">
                         <p>Se prepare, você vai participar de uma corrida secreta em busca de um tesouro enterrado no nosso prédio.</p>
                         <p>Você vai ter que desvendar pistas e seguir o caminho até o seu premio.</p>
-                        <p>Como todo Baú de Tesouro, existe apenas um ganhador, então se apresse e siga os passos logo para não ser um marujo atrasado!</p>
-                        <p>Ao clicar em "Ir à Caça" você receberá o primeiro desafio!</p>
+                        <p>Como toda saga pirata, existe apenas um ganhador, então se apresse e siga os passos logo para não ser um marujo atrasado!</p>
+                        <p>Ao clicar no Baú do Tesouro você receberá o primeiro desafio!</p>
                         <p>Boa sorte!</p>
                     </div>
-                    <Link className="link-button animation-grow" to="/pergunta-1">Ir à Caça</Link>
+                    <Link className="link-img animation-grow" to="/pergunta-1"><img className="img animation-grow" alt="Imagem Bau" src={bau}></img></Link>
                     <LogoMhedTech />
                 </div>
             </div>
